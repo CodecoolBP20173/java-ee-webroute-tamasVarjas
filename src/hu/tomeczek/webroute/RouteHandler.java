b4301e78e739085aeb5ff78df003924773f77e0f
tamasVarjas <tamas.varjas00@gmail.com>
2018-05-24 21:29:16 +0200

    add new POST handlers to RouteHandler

--- src/hu/tomeczek/webroute/RouteHandler.java
+++ src/hu/tomeczek/webroute/RouteHandler.java
@@ -4,15 +4,27 @@ import com.sun.net.httpserver.HttpExchange;
 
 public class RouteHandler {
 
-    @WebRoute("/")
-    void onRoot (HttpExchange httpExchange) {
-        String response = "This is the response for the root (\"/\") URL.";
+    @WebRoute(method = "GET", path = "/")
+    void onRootWithGet (HttpExchange httpExchange) {
+        String response = "This is the \"GET\" response for the root (\"/\") URL.";
         httpExchange.setAttribute("response", response);
     }
 
-    @WebRoute("/test")
-    void onTest (HttpExchange httpExchange) {
-        String response = "This is the response for the (\"/test\") URL.";
+    @WebRoute(method = "POST", path = "/")
+    void onRootWithPost (HttpExchange httpExchange) {
+        String response = "This is the \"POST\" response for the root (\"/\") URL.";
+        httpExchange.setAttribute("response", response);
+    }
+
+    @WebRoute(method = "GET", path = "/test")
+    void onTestWithGet (HttpExchange httpExchange) {
+        String response = "This is the \"GET\" response for the (\"/test\") URL.";
+        httpExchange.setAttribute("response", response);
+    }
+
+    @WebRoute(method = "POST", path = "/test")
+    void onTestWithPost (HttpExchange httpExchange) {
+        String response = "This is the \"POST\" response for the (\"/test\") URL.";
         httpExchange.setAttribute("response", response);
     }
 
